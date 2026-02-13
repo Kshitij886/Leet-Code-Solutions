8391383113857de4023de4a795395f14888a048f
kshitij khatri <kshitijkhatri437@gmail.com>
2026-02-13 22:33:44 +0545

    integer to roman problem start

--- src/bin/problemset12.rs
+++ src/bin/problemset12.rs
@@ -2,11 +2,19 @@ struct Solution {}
 
 impl Solution {
     pub fn int_to_roman(num: i32) -> String {
+        let vec1 = vec!["I", "V", "X", "L", "C", "D", "M"];
         let vec: Vec<u32> = num
             .to_string()
             .chars()
             .map(|c| c.to_digit(10).unwrap())
             .collect();
+        let len = vec.len();
+        if num > 3999 {
+            return "".to_string();
+        }
+        if len == 4 {
+
+        }
         return String::from("hello");
     }
 }
@@ -14,3 +22,12 @@ impl Solution {
 fn main() {
     println!("{}", Solution::int_to_roman(23));
 }
+
+
+// I	1
+// V	5
+// X	10
+// L	50
+// C	100
+// D	500
+// M	1000
