ca1f5bda1abfb3726b8d85cc69247ae1a4f463e9
Kshitij886 <kshitijkhatri437@gmail.com>
2026-02-18 14:00:51 +0545

    integer to roman

--- src/bin/problemset12.rs
+++ src/bin/problemset12.rs
@@ -1,22 +1,38 @@
 struct Solution {}
 
 impl Solution {
-    pub fn int_to_roman(num: i32) -> String {
-        let vec1 = vec!["I", "V", "X", "L", "C", "D", "M"];
-        let vec: Vec<u32> = num
-            .to_string()
-            .chars()
-            .map(|c| c.to_digit(10).unwrap())
-            .collect();
-        for i in 0..vec.len() {
-            let place = 10u32.pow(i as u32);
+    pub fn int_to_roman(mut num: i32) -> String {
+        if num == 0 {
+            return "".to_string();
         }
-        return String::from("hello");
+        let mapping = vec![
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ];
+        let mut result = String::new();
+        for (val, roman) in mapping {
+            while num >= val {
+                result.push_str(roman);
+                num -= val;
+            }
+        }
+        return result;
     }
 }
 
 fn main() {
-    println!("{}", Solution::int_to_roman(23));
+    println!("{}", Solution::int_to_roman(3749));
 }
 
 // I	1
