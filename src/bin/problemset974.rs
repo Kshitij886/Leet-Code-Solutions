0e6b8fb289cc9376b93524521743131526d4e51f
Kshitij886 <kshitijkhatri437@gmail.com>
2026-02-19 13:02:44 +0545

    problem set 974

--- src/bin/problemset974.rs
+++ src/bin/problemset974.rs
@@ -1,19 +1,36 @@
 struct Solution;
 
+use std::collections::HashMap;
+
 impl Solution {
     pub fn subarrays_div_by_k(nums: Vec<i32>, k: i32) -> i32 {
-        let mut vec: Vec<i32> = Vec::new();
-        for n in nums {
-            if n % k == 0 {
-                vec.push(n);
+        let mut count = 0;
+        let mut prefix_sum = 0;
+        let mut remainder_map = HashMap::new();
+
+        remainder_map.insert(0, 1);
+
+        for num in nums {
+            prefix_sum += num;
+
+            let mut remainder = prefix_sum % k;
+            if remainder < 0 {
+                remainder += k;
             }
+
+            if let Some(&freq) = remainder_map.get(&remainder) {
+                count += freq;
+            }
+
+            *remainder_map.entry(remainder).or_insert(0) += 1;
         }
 
-        return vec.len() as i32;
+        count
     }
 }
+
 fn main() {
-    let ans = vec![4, 5, 0, -2, -3, 1];
-    let k = 5;
+    let ans = vec![-2];
+    let k = 9;
     println!("{}", Solution::subarrays_div_by_k(ans, k));
 }
