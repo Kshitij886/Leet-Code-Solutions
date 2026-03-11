e66bb76384abd814fe7281685271893f4bbf11b7
Kshitij886 <kshitijkhatri437@gmail.com>
2026-03-11 16:44:16 +0545

    problem set 43

--- src/bin/problemset43.rs
+++ src/bin/problemset43.rs
@@ -1,27 +1,41 @@
 struct Solution;
 impl Solution {
     fn multiply(num1: String, num2: String) -> String {
-        let nums = vec![('1',1),('2',2), ('3',3),('4',4),('5',5),('5',6),('7',7),('8',8),('9',9),('0',0)];
-        let mut num3: i32 = 0;
-        let mut num4: i32 = 0;
-        for (str, num) in &nums {
-            for n in num1.chars() {
-                if &n == str {
-                    num3 = num3 *10 + num;
-                }
+        if num1 == "0" || num2 == "0" {
+            return "0".to_string();
+        }
+
+        let n = num1.len();
+        let m = num2.len();
+        let mut result = vec![0; n + m];
+
+        let a: Vec<u8> = num1.bytes().collect();
+        let b: Vec<u8> = num2.bytes().collect();
+
+        for i in (0..n).rev() {
+            for j in (0..m).rev() {
+                let mul = (a[i] - b'0') as i32 * (b[j] - b'0') as i32;
+                let sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
             }
         }
-        for (str, num) in &nums {
-            for n in num2.chars() {
-                if &n == str {
-                    num4 = num4 *10 + num;
-                }
-            }
-        }
-        return (num3 * num4).to_string();
+
+        result
+            .into_iter()
+            .skip_while(|&x| x == 0)
+            .map(|x| (x as u8 + b'0') as char)
+            .collect()
     }
 }
 
 fn main() {
-    println!("{}", Solution::multiply(String::from("98"), String::from("9")));
+    println!(
+        "{}",
+        Solution::multiply(
+            String::from("498828660196"),
+            String::from("167141802233061013023557397451289113296441069")
+        )
+    );
 }
