3f06a83bc80d50ee7a399d6f3554c80c26859cb7
Kshitij886 <kshitijkhatri437@gmail.com>
2026-03-11 12:45:22 +0545

    solve problem set 13

--- src/bin/problemset13.rs
+++ src/bin/problemset13.rs
@@ -1,24 +1,36 @@
 struct Solution;
 
 impl Solution {
-    pub fn roman_to_int(s: String) -> i32 {
+    pub fn roman_to_int(mut s: String) -> i32 {
         let mut num: i32 = 0;
         let mapping = vec![
             (1000, "M"),
-            (900, "CM"),
+            // (900, "CM"),
             (500, "D"),
-            (400, "CD"),
+            // (400, "CD"),
             (100, "C"),
-            (90, "XC"),
+            // (90, "XC"),
             (50, "L"),
-            (40, "XL"),
+            // (40, "XL"),
             (10, "X"),
-            (9, "IX"),
+            // (9, "IX"),
             (5, "V"),
-            (4, "IV"),
+            // (4, "IV"),
             (1, "I"),
         ];
-        for c in s.chars() {}
+        s = s.replace("CM", "DCCCC");
+        s = s.replace("CD", "CCCC");
+        s = s.replace("XC", "LXXXX");
+        s = s.replace("XL", "XXXX");
+        s = s.replace("IX", "VIIII");
+        s = s.replace("IV", "IIII");
+        for (value, roman) in mapping {
+            for c in s.chars() {
+                if c.to_string() == roman {
+                    num += value;
+                }
+            }
+        }
         return num;
     }
 }
