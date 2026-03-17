3c007fff2f0be8c51c7587b546475285402063e0
Kshitij886 <kshitijkhatri437@gmail.com>
2026-03-17 16:21:50 +0545

    remaining ps 60

--- src/bin/problemset60.rs
+++ src/bin/problemset60.rs
@@ -1,16 +1,26 @@
 struct Solution;
 
 impl Solution {
-    pub fn get_permutation(n: i32, k: i32) -> String {
-        if n > 9 || n < 1 || {
-            return "".to_string();
+    pub fn get_permutation(n: i32, mut k: i32) -> String {
+        let mut num = 0;
+        let mut arr: Vec<i32> = Vec::new();
+        for i in 1..n + 1 {
+            arr.push(i);
         }
-        return String::from("hello");
+        println!("{:?}", arr);
+        // while k > 0 {
+        //     num = 0;
+        //     for i in 1..n + 1 {
+        //         num = num * 10 + i;
+        //     }
+        //     k -= 1;
+        // }
+        return num.to_string();
     }
 }
 
 fn main() {
     let n = 3;
-    let k = 3;
+    let mut k = 3;
     println!("Solution is {}", Solution::get_permutation(n, k));
 }
