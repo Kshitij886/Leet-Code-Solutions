3dc73e4dfb9bcdb50d3d387bbeb491ac334eadae
Kshitij886 <kshitijkhatri437@gmail.com>
2026-03-18 15:56:56 +0545

    problem set 8 started

---
+++ src/bin/problemset8.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+struct Solution;
+
+impl Solution {
+    fn my_atoi(s: String) -> i32 {
+        return 43;
+    }
+}
+
+fn main() {
+    let s = "42".to_string();
+    println!("Ans {}", Solution::my_atoi(s))
+}
