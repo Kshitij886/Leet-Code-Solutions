ff494e48018b415e4e2d8e157b4de37022c181fb
Kshitij886 <kshitijkhatri437@gmail.com>
2026-03-19 12:43:39 +0545

    hello

--- src/bin/problemset10.rs
+++ src/bin/problemset10.rs
@@ -2,32 +2,12 @@ struct Solution;
 
 impl Solution {
     fn is_match(s: String, p: String) -> bool {
-        let mut new_str = String::from("");
-        let mut prev_str: char = ' ';
-        if s.len() != p.len() && (!p.contains("*") || !p.contains(".")) {
-            return false;
-        }
-        for c in s.chars() {
-            for b in p.chars() {
-                if b == '.' {
-                    new_str.push(c);
-                } else if b == '*' {
-                    new_str.push(prev_str);
-                } else if b == c {
-                    new_str.push(c);
-                }
-                prev_str = c;
-            }
-        }
-        if new_str == s {
-            return true;
-        }
         return false;
     }
 }
 
 fn main() {
-    let s = String::from("aa");
-    let p = String::from("a*");
+    let s = String::from("aaaaaaaaaaaaab");
+    let p = String::from("a*a*a*a*a*a*a*a*a*a*b");
     println!("{}", Solution::is_match(s, p));
 }
