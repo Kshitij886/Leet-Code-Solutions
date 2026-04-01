51599c4ec05b537e7041278352dec6350b4fcb49
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-01 14:42:52 +0545

    prefix sum

--- src/bin/prefixsum.rs
+++ src/bin/prefixsum.rs
@@ -1,18 +1,18 @@
+struct Solution;
 
-
+impl Solution {
+    fn prefix_sum() -> Vec<i32> {
+        let arr = vec![1, 2, 3, 4, 5];
+        let mut prefix_sum_arr: Vec<i32> = Vec::new();
+        prefix_sum_arr.push(arr[0]);
+        for i in 1..arr.len() {
+            prefix_sum_arr.push(prefix_sum_arr[i - 1] + arr[i]);
+        }
+        return prefix_sum_arr;
+    }
+}
 
 fn main() {
     println!("hello");
-    prefix_sum();
-}
-
-
-fn prefix_sum() -> Vec<i32>{
-    let  arr = vec![1,2,3,4,5];
-    let mut prefix_sum_arr: Vec<i32> = Vec::new();
-    prefix_sum_arr.push(arr[0]);
-    for i in 1..arr.len() {
-        prefix_sum_arr.push(prefix_sum_arr[i-1] + arr[i]);
-    }
-    return prefix_sum_arr;
+    Solution::prefix_sum();
 }
