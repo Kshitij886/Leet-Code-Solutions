9e6077ed09fbb73b241b56bfa78538c2854db66c
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-16 15:09:41 +0545

    initialize problem set 6

---
+++ src/bin/problemset6.rs
@@ -0,0 +1,11 @@
+struct Solution;
+
+impl Solution {
+    fn convert(s: String) -> String {
+        return "".to_string();
+    }
+}
+fn main() {
+    let s: String = String::from("");
+    println!("ans: {}", Solution::convert(s));
+}
