4083b4f58afa189f1cb3a2e8bd30131c42c4be42
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-16 15:49:07 +0545

    problem set 6 solved

--- src/bin/problemset6.rs
+++ src/bin/problemset6.rs
@@ -1,12 +1,27 @@
+#![allow(unused)]
 struct Solution;
 
 impl Solution {
     fn convert(s: String, n: i32) -> String {
-        return "".to_string();
+        let chars: Vec<char> = s.chars().collect();
+        let len = chars.len();
+        if len == 1 || len == 2 {
+            return s;
+        }
+        let diff = 2 * (n - 1);
+        let mut sub_string = Vec::<char>::new();
+        for i in 0..chars.len() {
+            let mut indx = diff * i as i32;
+            if indx > len as i32 {
+                indx = indx % len as i32;
+            }
+            sub_string.push(chars[indx as usize]);
+        }
+        return sub_string.into_iter().collect();
     }
 }
 fn main() {
-    let s: String = String::from("");
-    let n = 32;
+    let s: String = String::from("PAYPALISHIRING");
+    let n = 3;
     println!("ans: {}", Solution::convert(s, n));
 }
