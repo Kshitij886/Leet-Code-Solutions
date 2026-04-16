f77f5fbd796e2ed582cea25cf1d514fbbaae40ef
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-16 15:04:19 +0545

    problem set 3 solved

--- src/bin/problemset3.rs
+++ src/bin/problemset3.rs
@@ -1,11 +1,27 @@
+#![allow(unused)]
+use std::collections::HashSet;
+
 struct Solution;
 impl Solution {
     fn length_of_longest_substring(s: String) -> i32 {
-        return 32;
+        let chars: Vec<char> = s.chars().collect();
+        let mut set = HashSet::<char>::new();
+        let mut left = 0;
+        let mut max = 0;
+        for right in 0..chars.len() {
+            while set.contains(&chars[right]) {
+                set.remove(&chars[left]);
+                left += 1;
+            }
+            set.insert(chars[right]);
+            max = max.max((right - left + 1) as i32)
+        }
+
+        max
     }
 }
 
 fn main() {
-    let sub_string = "".to_string();
+    let sub_string = "bbbbb".to_string();
     println!("ans: {}", Solution::length_of_longest_substring(sub_string));
 }
