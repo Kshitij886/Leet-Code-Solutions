177d6694ebac0702daa782535df71b5ecdcd09db
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-16 14:42:28 +0545

    initialize problem set 3

---
+++ src/bin/problemset3.rs
@@ -0,0 +1,11 @@
+struct Solution;
+impl Solution {
+    fn length_of_longest_substring(s: String) -> i32 {
+        return 32;
+    }
+}
+
+fn main() {
+    let sub_string = "".to_string();
+    println!("ans: {}", Solution::length_of_longest_substring(sub_string));
+}
