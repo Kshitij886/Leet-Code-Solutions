c234c8d37d5e8b02d1a02dc92246539ac4284505
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-17 15:40:56 +0545

    initialize problem set 15

---
+++ src/bin/problemset15.rs
@@ -0,0 +1,14 @@
+#![allow(unused)]
+struct Solution;
+
+impl Solution {
+    fn three_sum(num: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut ans = Vec::<Vec<i32>>::new();
+        return ans;
+    }
+}
+
+fn main() {
+    let nums = vec![-1, 0, 1, 2, -1, -4];
+    println!("ans : {:?}", Solution::three_sum(nums));
+}
