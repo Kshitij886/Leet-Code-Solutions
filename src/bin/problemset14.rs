336845a9ec10db5bc192c3b3ad4fd3baf0e085b3
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-17 15:34:10 +0545

    problem set 14 solution

--- src/bin/problemset14.rs
+++ src/bin/problemset14.rs
@@ -2,13 +2,23 @@
 struct Solution;
 
 impl Solution {
-    fn longest_common_prefix(str: Vec<String>) -> String {
-        let mut ans = String::new();
-        let temp_str = String::new();
-        for name in str {
-            //
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        if strs.is_empty() {
+            return "".to_string();
         }
-        return "he".to_string();
+
+        let mut prefix = strs[0].clone();
+
+        for s in strs.iter().skip(1) {
+            while !s.starts_with(&prefix) {
+                prefix.pop();
+                if prefix.is_empty() {
+                    return "".to_string();
+                }
+            }
+        }
+
+        prefix
     }
 }
 fn main() {
