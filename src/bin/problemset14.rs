ef0c4a53fc4d8c0bad3731c2606089070d94f233
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-17 15:16:14 +0545

    initialize problem set 14

---
+++ src/bin/problemset14.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+struct Solution;
+
+impl Solution {
+    fn longest_common_prefix(str: Vec<String>) -> String {
+        let mut ans = String::new();
+        let temp_str = String::new();
+        for name in str {
+            //
+        }
+        return "he".to_string();
+    }
+}
+fn main() {
+    let s = vec![
+        "flower".to_string(),
+        "flow".to_string(),
+        "flight".to_string(),
+    ];
+    println!("ans: {}", Solution::longest_common_prefix(s));
+}
